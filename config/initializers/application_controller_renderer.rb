4ece992e1fbad4d143137ee8f3c5cea7bb84a941
Atsuya2000 <atuyajyun@gmail.com>
2021-03-11 10:12:26 +0000

    finishcommit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '2402ad59d54b95f2e9d9385867e209c6a459932a7fe5f4c5e078a22e919073efb56185b36780dbddf645ab36aa0226066b2bf0256426de772af6e062fbd5614f'
